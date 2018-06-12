2967769615949150e4f161cd294faef1e99cb7e3
Narayanan Ts <tsnarayanan7@gmail.com>
2018-06-12 17:11:16 +0530

    inheritance

--- 4 inheritance.js
+++ 4 inheritance.js
@@ -4,21 +4,21 @@
     Constructor
  */
 
- function book(title,author,year){
-     this.title = title;
-     this.author = author;
-     this.year = year;
- }
+function book(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+}
 
 /** Prototype Function */
 
-book.prototype.getSummary = function(){
+book.prototype.getSummary = function () {
     return `${this.title} was written by ${this.author} in ${this.year}`;
 };
 
 // Magazine Constructor
-function magazine(title,author,year,month){
-    book.call(this,title,author,year);
+function magazine(title, author, year, month) {
+    book.call(this, title, author, year);
     this.month = month;
 }
 
@@ -26,7 +26,7 @@ function magazine(title,author,year,month){
 magazine.prototype = Object.create(book.prototype);
 
 // Instantiate Magazine Object
-const mag1 = new magazine('Mag One','John Doe','2018','May');
+const mag1 = new magazine('Mag One', 'John Doe', '2018', 'May');
 
 
 //use magazine constructor
