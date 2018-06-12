e2a87557b3bbbf03594fa9698f8b3dfe4fca3904
Narayanan Ts <tsnarayanan7@gmail.com>
2018-06-12 17:11:05 +0530

    constructor

--- 2 constructor.js
+++ 2 constructor.js
@@ -4,19 +4,19 @@
     Constructor
  */
 
- function book(title,author,year){
-     this.title = title;
-     this.author = author;
-     this.year = year;
-     this.displayBook = function(){
-         return `${this.title} was written by ${this.author} in ${this.year}`;
-     }
- }
+function book(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.displayBook = function () {
+        return `${this.title} was written by ${this.author} in ${this.year}`;
+    }
+}
 
 // Instantiate an Object
- const book1 = new book('Book 1','John Doe','1993');
- console.log(book1.displayBook());
+const book1 = new book('Book 1', 'John Doe', '1993');
+console.log(book1.displayBook());
 
 // New Object instantiated
- const book2 = new book('Book 2','Jane Doe','2018');
- console.log(book2.displayBook());
+const book2 = new book('Book 2', 'Jane Doe', '2018');
+console.log(book2.displayBook());
