7ba8b1664b921aa098ef3d900193bc54486185e9
Narayanan <narayanan.s@tvsnext.io>
2018-06-12 11:44:18 +0530

    constructor

---
+++ 2 constructor.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+    Constructor
+ */
+
+ function book(title,author,year){
+     this.title = title;
+     this.author = author;
+     this.year = year;
+     this.displayBook = function(){
+         return `${this.title} was written by ${this.author} in ${this.year}`;
+     }
+ }
+
+// Instantiate an Object
+ const book1 = new book('Book 1','John Doe','1993');
+ console.log(book1.displayBook());
+
+// New Object instantiated
+ const book2 = new book('Book 2','Jane Doe','2018');
+ console.log(book2.displayBook());
